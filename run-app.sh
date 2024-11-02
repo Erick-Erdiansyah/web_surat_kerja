dc68b2bae6527d317d30a5205d788cd8d361b200
Erick-Erdiansyah <erdiansyah290@gmail.com>
2024-11-03 03:56:53 +0700

    forget the seed -_-

--- run-app.sh
+++ run-app.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 # Make sure this file has executable permissions, run `chmod +x run-app.sh`
-
-
-# seed database
-php artisan migrate --force
-
-#install package
-npm install
-
-# Build assets using NPM
-npm run build
-
-# symlink
-php artisan storage:link
-# Clear cache
-php artisan optimize:clear
-
-# Cache the various components of the Laravel application
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
+# Run migrations, process the Nginx configuration template and start Nginx
+php artisan migrate --seed --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
